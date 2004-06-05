54db92d5106d3e009ce413fa7a12227ce7d8f64e
crac <crac>
2004-06-05 18:31:39 +0000

    get Repository from DataBus.

--- MediaManager/ch/fha/mediamanager/data/QueryRequest.java
+++ MediaManager/ch/fha/mediamanager/data/QueryRequest.java
@@ -7,12 +7,9 @@ import java.util.Vector;
 /**
  *
  * @author crac
- * @version $Id: QueryRequest.java,v 1.8 2004/06/05 14:13:06 crac Exp $
+ * @version $Id: QueryRequest.java,v 1.9 2004/06/05 18:31:39 crac Exp $
  */
 public class QueryRequest {
-    
-    // TODO: has to be auto-loaded from config
-    private Repository repository = new DatabaseRepository();
     
     // 
     public static final int OR = 0;           // +
@@ -25,15 +22,16 @@ public class QueryRequest {
     public static final int UPDATE = 1;
     public static final int LOAD = 2;
     public static final int DELETE = 3;
-    public static int ENTITY_CREATE = 4;
-    public static int ENTITY_DELETE = 5;
-    public static int FIELD_ADD = 6;
-    public static int FIELD_DELETE = 7;
+    public static final int ENTITY_CREATE = 4;
+    public static final int ENTITY_DELETE = 5;
+    public static final int FIELD_ADD = 6;
+    public static final int FIELD_DELETE = 7;
     
     // --------------------------------
     // ATTRIBUTES
     // --------------------------------
 
+    private Repository repository;
     private DataSet dSet;
     private int type;
     private Vector request;
@@ -45,10 +43,18 @@ public class QueryRequest {
     
     /**
      * 
+     */
+    public QueryRequest() {
+    	this.repository = DataBus.getRepository();
+    }
+    
+    /**
+     * 
      * @param vec
      * @param type
      */
     public QueryRequest(Vector vec, int type) {
+        this();
         parse(vec);
         this.type = type;
         this.request = vec;
@@ -60,9 +66,14 @@ public class QueryRequest {
      * @param type
      */
     public QueryRequest(DataSet dSet, int type) {
+        this();
         this.dSet = dSet;
         this.type = type;
     }
+    
+    // --------------------------------
+    // OPERATIONS
+    // --------------------------------
     
     /**
      * Parses the request vector and checks the
