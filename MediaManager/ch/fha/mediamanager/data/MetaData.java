d68056e0e178d9a7d69997f2ee731fd3183ccdbe
crac <crac>
2004-06-05 20:13:55 +0000

    added Set operations.

--- MediaManager/ch/fha/mediamanager/data/MetaData.java
+++ MediaManager/ch/fha/mediamanager/data/MetaData.java
@@ -1,10 +1,11 @@
 package ch.fha.mediamanager.data;
 
+import java.util.Iterator;
 import java.util.Set;
 
 /**
  * @author crac
- * @version $Id: MetaData.java,v 1.1 2004/06/05 18:20:47 crac Exp $
+ * @version $Id: MetaData.java,v 1.2 2004/06/05 20:13:55 crac Exp $
  */
 public class MetaData {
     
@@ -22,6 +23,91 @@ public class MetaData {
     // --------------------------------
     // OPERATIONS
     // --------------------------------
+    
+    /**
+     * 
+     * @param o
+     */
+    public void add(Object o) {
+    	if (o instanceof MetaEntity) {
+            addEntity((MetaEntity) o);
+        }
+        else if (o instanceof MetaField) {
+        	addField((MetaField) o);
+        }
+    }
+    
+    /**
+     * 
+     * @param entity
+     */
+    public void addEntity(MetaEntity entity) {
+    	metaEntities.add(entity);
+    }
+    
+    /**
+     * 
+     * @param field
+     */
+    public void addField(MetaField field) {
+    	metaFields.add(field);
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public Iterator entityIterator() {
+    	return metaEntities.iterator();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public Iterator fieldIterator() {
+    	return metaFields.iterator();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public boolean isEmpty() {
+    	return entityIsEmpty() && fieldIsEmpty();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public boolean entityIsEmpty() {
+    	return metaEntities.isEmpty();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public boolean fieldIsEmpty() {
+    	return metaFields.isEmpty();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public int entitySize() {
+    	return metaEntities.size();
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public int fieldSize() {
+    	return metaFields.size();
+    }
     
     // --------------------------------
     // ACCESSORS
