534cee0c0043efc7032caed1ff94079a1c928a91
crac <crac>
2004-06-11 12:47:36 +0000

    *** empty log message ***

--- MediaManager/ch/fha/mediamanager/data/User.java
+++ MediaManager/ch/fha/mediamanager/data/User.java
@@ -3,25 +3,54 @@ package ch.fha.mediamanager.data;
 /**
  *
  * @author crac
- * @version $Id: User.java,v 1.1 2004/05/20 14:40:43 crac Exp $
+ * @version $Id: User.java,v 1.2 2004/06/11 12:47:36 crac Exp $
  */
-public class User {
+public final class User {
     
-    private String uuId;
+    // --------------------------------
+    // FIELDS
+    // --------------------------------
+    
+    private String uuId; // immutable
     private String name;
     
+    // --------------------------------
+    // CONSTRUCTORS
+    // --------------------------------
+    
+    // --------------------------------
+    // OPERATIONS
+    // --------------------------------
+    
+    // --------------------------------
+    // ACCESSORS
+    // --------------------------------
+    
+    /**
+     * 
+     * @return
+     */
     public String getUUID() {
         return uuId;
     }
 
+    /**
+     * 
+     * @return
+     * @return
+     */
     public String getName() {
         return name;
     }
+    
+    // --------------------------------
+    // MUTATORS
+    // --------------------------------
 
-    public void setUUID(String uuId) {
-        this.uuId = uuId;
-    }
-
+    /**
+     * 
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
