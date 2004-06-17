6a97b3a6ce19b8fbac421616c1e30b157c293e3b
ia02vond <ia02vond>
2004-06-17 14:29:35 +0000

    added method getDataElement()

--- MediaManager/ch/fha/mediamanager/plugin/MMPluginEvent.java
+++ MediaManager/ch/fha/mediamanager/plugin/MMPluginEvent.java
@@ -1,13 +1,24 @@
 package ch.fha.mediamanager.plugin;
 
+import ch.fha.mediamanager.data.DataElement;
 import ch.fha.pluginstruct.*;
 
 /**
  * @author ia02vond
- * @version $Id: MMPluginEvent.java,v 1.1 2004/05/13 12:10:08 ia02vond Exp $
+ * @version $Id: MMPluginEvent.java,v 1.2 2004/06/17 14:29:35 ia02vond Exp $
  */
 public class MMPluginEvent extends PluginEvent {
 	
+	private DataElement dataElement;
 	
+	public MMPluginEvent() {
+	}
 	
+	public MMPluginEvent(DataElement dataElement) {
+		this.dataElement = dataElement;
+	}
+	
+	public DataElement getDataElement() {
+		return dataElement;
+	}
 }
