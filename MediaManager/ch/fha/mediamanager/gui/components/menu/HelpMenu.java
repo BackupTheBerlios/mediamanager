708f11916cc5c0e3cd4f08cb4c1243cd7d1efe3d
radisli <radisli>
2004-06-21 07:43:58 +0000

    Code changed ActionListener --> ActionHandler

--- MediaManager/ch/fha/mediamanager/gui/components/menu/HelpMenu.java
+++ MediaManager/ch/fha/mediamanager/gui/components/menu/HelpMenu.java
@@ -1,15 +1,15 @@
-//$Id: HelpMenu.java,v 1.1 2004/06/16 08:09:49 radisli Exp $
+//$Id: HelpMenu.java,v 1.2 2004/06/21 07:43:58 radisli Exp $
 package ch.fha.mediamanager.gui.components.menu;
 
 import javax.swing.*;
-import java.awt.event.*;
 
 import ch.fha.mediamanager.gui.*;
+import ch.fha.mediamanager.gui.framework.*;
 
 public class HelpMenu extends JMenu {
 	public HelpMenu() {
 		MainFrame mainWindow = MainFrame.getInstance();
-		ActionListener mainActionListener = mainWindow.getMainActionListener();
+		ActionHandler mainActionListener = mainWindow.getMainActionListener();
 		
 		setText("Help");
 		JMenuItem about = new JMenuItem("About");
