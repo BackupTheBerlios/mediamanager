325b54d1f616d62251c85507948ed2eb1a910280
radisli <radisli>
2004-06-21 07:29:09 +0000

    Removed additional parameter (String) in case of never used. No distinction between KeyPointEvents in the ActionHAndler anymore - Events must be distinguisch in the runAction method Removed in case of performance

--- MediaManager/ch/fha/mediamanager/gui/framework/ActionHandler.java
+++ MediaManager/ch/fha/mediamanager/gui/framework/ActionHandler.java
@@ -1,4 +1,4 @@
-//$Id: ActionHandler.java,v 1.2 2004/06/16 08:10:36 radisli Exp $
+//$Id: ActionHandler.java,v 1.3 2004/06/21 07:29:09 radisli Exp $
 package ch.fha.mediamanager.gui.framework;
 
 import java.util.*;
@@ -16,7 +16,7 @@ import ch.fha.mediamanager.gui.MainFrame;
 public class ActionHandler implements 
 	ActionListener
 {
-	HashMap map = new HashMap();
+	LinkedList list = new LinkedList();
 	
 	/**
 	 * Adds a <code>KeyPointListener</code> to the <code>map</code> list.
@@ -24,11 +24,9 @@ public class ActionHandler implements
 	 * crossings of keypoints. 
 	 * 
 	 * @param l is the <code>KeyPointListener</code> to add.
-	 * @param e is the <code>KeyPointEvent</code> which belongs to
-	 *        the <code>KeyPointListener</code>.
 	 */
-	public void addActionListener(KeyPointListener l, KeyPointEvent e){
-		map.put(l, e);
+	public void addActionListener(KeyPointListener l) {
+		list.add(l);
 	}
 
 	/**
@@ -38,8 +36,8 @@ public class ActionHandler implements
 	 * 
 	 * @param l is the <code>KeyPointListener</code> to remove.
 	 */
-	public void removeModelChangeListener(KeyPointListener l){
-		map.remove((Object)l);
+	public void removeModelChangeListener(KeyPointListener l) {
+		list.remove(l);
 	}
 	
 	/**
@@ -50,16 +48,10 @@ public class ActionHandler implements
 	 * @param fireKey is a predefined keypoint-action.
 	 */
 	public void fireAction(int fireKey) {
-		Set s = map.keySet();
-		Iterator it = s.iterator();
-		Object obj;
-		KeyPointEvent e;
+		Iterator it = list.iterator();
+		KeyPointEvent e = new KeyPointEvent(fireKey);
 		while(it.hasNext()) {
-			obj = it.next();
-			e = (KeyPointEvent)map.get(obj);
-			if(e.alertOnKey == fireKey) {
-				((KeyPointListener)obj).runAction(e);
-			}
+			((KeyPointListener)it.next()).runAction(e);
 		}
 	}
 
--- MediaManager/ch/fha/mediamanager/gui/framework/KeyPointEvent.java
+++ MediaManager/ch/fha/mediamanager/gui/framework/KeyPointEvent.java
@@ -1,4 +1,4 @@
-//$Id: KeyPointEvent.java,v 1.2 2004/06/18 11:07:38 radisli Exp $
+//$Id: KeyPointEvent.java,v 1.3 2004/06/21 07:29:09 radisli Exp $
 package ch.fha.mediamanager.gui.framework;
 
 /**
@@ -10,32 +10,26 @@ public class KeyPointEvent {
 	/** Key Points */
 	public static final int WINDOW_EXIT = 1;
 	public static final int CONFIG_PANEL_LOAD = 2;
+	public static final int CONNECTING = 3;
+	public static final int DISCONNECTING = 4;
 	
 	/** Event responds only on this action */
 	public final int alertOnKey;
-	
-	/** Additional Parameters */
-	private String parameter;
 	
 	/**
 	 * Event constructor
 	 * 
 	 * @param alertOnKey defines on what kind of event it should respond
 	 */
-	public KeyPointEvent(int alertOnKey, String parameter) {
+	public KeyPointEvent(int alertOnKey) {
 		this.alertOnKey = alertOnKey;
-		this.parameter = parameter;
 	}
 	
 	/**
-	 * Gets the parameter
+	 * Gets the <code>alertOnKey</code> which specifies the action 
 	 * 
-	 * @return parameter which gives some additional informations
+	 * @return action number
 	 */
-	public String getParameter() {
-		return parameter;
-	}
-	
 	public int getKeyPointEvent() {
 		return alertOnKey;
 	}	
