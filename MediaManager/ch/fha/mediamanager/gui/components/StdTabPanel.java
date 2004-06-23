eff446d929cc8f21790f6fb46ef7bac14d004410
ia02vond <ia02vond>
2004-06-23 10:47:34 +0000

    extended

--- MediaManager/ch/fha/mediamanager/gui/components/StdTabPanel.java
+++ MediaManager/ch/fha/mediamanager/gui/components/StdTabPanel.java
@@ -1,22 +1,30 @@
-//$Id: StdTabPanel.java,v 1.4 2004/06/21 05:53:02 radisli Exp $
+//$Id: StdTabPanel.java,v 1.5 2004/06/23 10:47:34 ia02vond Exp $
 package ch.fha.mediamanager.gui.components;
 
 import java.awt.*;
 import javax.swing.*;
 import java.util.prefs.*;
 
+import ch.fha.mediamanager.data.MetaEntity;
 import ch.fha.mediamanager.gui.*;
 import ch.fha.mediamanager.gui.framework.*;
+import ch.fha.mediamanager.gui.util.DataTableModel;
 
 public class StdTabPanel extends JPanel implements
 	Savable
 {
-	public StdTabPanel() {
+	
+	private JTable table;
+	
+	public StdTabPanel(MetaEntity metaEntity) {
 		Mediamanager.addSavable(this);
 		setLayout(new BorderLayout());
 		MainFrame mainWindow = MainFrame.getInstance();
 		ActionHandler mainActionListener = mainWindow.getMainActionListener();
 		setBorder(new BarBorder("TEST-Panel"));
+		
+		//table = new JTable(new DataTableModel(metaEntity));
+		//add(table, BorderLayout.CENTER);
 		
 		JPanel defaultButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JButton exit = new JButton("Exit");
