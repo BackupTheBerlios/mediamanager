c463bcc1222c04781c561576e05588decc8dde82
ia02vond <ia02vond>
2004-06-23 10:45:35 +0000

    added method createEntityTabs()

--- MediaManager/ch/fha/mediamanager/gui/components/MainTabPanel.java
+++ MediaManager/ch/fha/mediamanager/gui/components/MainTabPanel.java
@@ -1,24 +1,39 @@
-// $Id: MainTabPanel.java,v 1.2 2004/06/17 12:35:04 radisli Exp $
+// $Id: MainTabPanel.java,v 1.3 2004/06/23 10:45:35 ia02vond Exp $
 package ch.fha.mediamanager.gui.components;
 
 import java.awt.*;
+import java.util.Iterator;
+
 import javax.swing.*;
+
+import ch.fha.mediamanager.data.DataBus;
+import ch.fha.mediamanager.data.MetaEntity;
 
 /**
  * The main view of the <em>Mediamanager</em> application.
  *
  * @author Roman Rietmann
  */
-public class MainTabPanel extends JPanel {	
+public class MainTabPanel extends JPanel {
+	
+	private JTabbedPane tabbedPane;
+	
 	public MainTabPanel() {
 		setLayout(new BorderLayout());
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		JPanel mainTab = new MainTab();
-		JPanel moviesTab = new StdTabPanel();
 		tabbedPane.addTab("Hauptfenster", mainTab);
-		tabbedPane.addTab("Movies", moviesTab);
-	
+		
 		add(tabbedPane, BorderLayout.CENTER);
+	}
+	
+	public void createEntityTabs() {	
+		Iterator it = DataBus.getMetaEntities().iterator();
+		MetaEntity metaEntity;
+		while (it.hasNext()) {
+			metaEntity = (MetaEntity)it.next();
+			tabbedPane.add(metaEntity.getName(), new StdTabPanel(metaEntity));
+		}
 	}
 }
