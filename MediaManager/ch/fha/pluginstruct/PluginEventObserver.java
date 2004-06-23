78d0036d0d604edfd980552c049cfd41882951b2
ia02vond <ia02vond>
2004-06-23 22:55:53 +0000

    initial import

---
+++ MediaManager/ch/fha/pluginstruct/PluginEventObserver.java
@@ -0,0 +1,13 @@
+package ch.fha.pluginstruct;
+
+/**
+ * @author ia02vond
+ * @version $Id: PluginEventObserver.java,v 1.1 2004/06/23 22:55:53 ia02vond Exp $
+ */
+public class PluginEventObserver {
+
+	public Plugin plugin;
+	public String condition;
+	public String event;
+
+}
