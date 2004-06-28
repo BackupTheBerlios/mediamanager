24bca2ff8ab7685bf7f2c63c66a96e16c6041130
radisli <radisli>
2004-06-28 08:08:32 +0000

    new layout

--- MediaManager/ch/fha/mediamanager/gui/components/AboutPanel.java
+++ MediaManager/ch/fha/mediamanager/gui/components/AboutPanel.java
@@ -1,4 +1,4 @@
-//$Id: AboutPanel.java,v 1.1 2004/06/16 08:09:49 radisli Exp $
+//$Id: AboutPanel.java,v 1.2 2004/06/28 08:08:32 radisli Exp $
 package ch.fha.mediamanager.gui.components;
 
 import java.awt.*;
@@ -20,24 +20,32 @@ public class AboutPanel extends JPanel {
 		MainFrame mainWindow = MainFrame.getInstance();
 		setLayout(new BorderLayout());
 		ActionListener mainActionListener = mainWindow.getMainActionListener();
+		setBorder(new BarBorder("About"));
 
-		Color bg = new Color(194, 242, 242);
-		JPanel aboutHolder = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JPanel aboutHolder = new JPanel();
+		GridBagLayout gridbag = new GridBagLayout();
+		aboutHolder.setLayout(gridbag);
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.anchor = GridBagConstraints.CENTER;
+
 		JLabel aboutLabel = new JLabel();
 		ImageIcon aboutImage = new ImageIcon("images/about.jpg");
 		Dimension d = new Dimension(aboutImage.getIconWidth(), aboutImage.getIconHeight());
 		aboutHolder.setPreferredSize(d);
 		aboutLabel.setIcon(aboutImage);
 		aboutHolder.add(aboutLabel);
-		aboutHolder.setBackground(bg);
-		add(aboutHolder, BorderLayout.CENTER);
-		
+		gridbag.setConstraints(aboutLabel, c);
+		aboutHolder.add(aboutLabel);
+		JScrollPane sp = new JScrollPane(aboutHolder);
+		sp.setBorder(null);
+		add(sp, BorderLayout.CENTER);
+	
 		JPanel defaultButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JButton back = new JButton("Zur\u00fcck");
 		back.setActionCommand("tabs");
 		back.addActionListener(mainActionListener);
 		defaultButtonPanel.add(back);
-		defaultButtonPanel.setBackground(bg);
 		add(defaultButtonPanel, BorderLayout.SOUTH);
 	}
 }
