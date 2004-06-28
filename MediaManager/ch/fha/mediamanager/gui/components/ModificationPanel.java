042d30e0bf11a141358f5bf544afe0f4b54fcafc
radisli <radisli>
2004-06-28 10:37:23 +0000

    button added

--- MediaManager/ch/fha/mediamanager/gui/components/ModificationPanel.java
+++ MediaManager/ch/fha/mediamanager/gui/components/ModificationPanel.java
@@ -1,4 +1,4 @@
-//$Id: ModificationPanel.java,v 1.4 2004/06/27 20:31:24 radisli Exp $
+//$Id: ModificationPanel.java,v 1.5 2004/06/28 10:37:23 radisli Exp $
 package ch.fha.mediamanager.gui.components;
 
 import java.awt.*;
@@ -8,8 +8,6 @@ import ch.fha.mediamanager.gui.*;
 import ch.fha.mediamanager.gui.framework.*;
 
 public class ModificationPanel extends JPanel {
-	private JButton exit, loadConfig;
-	
 	public ModificationPanel() {
 		MainFrame mainWindow = MainFrame.getInstance();
 		ActionHandler mainActionListener = mainWindow.getMainActionListener();
@@ -18,15 +16,20 @@ public class ModificationPanel extends JPanel {
 		
 		JPanel modPanel = new JPanel();
 		modPanel.setLayout(new BoxLayout(modPanel, BoxLayout.Y_AXIS));
-		loadConfig = new JButton("Einstellungen");
+		JButton loadMain = new JButton("Hauptfenster");
+		loadMain.setActionCommand("tabs");
+		loadMain.addActionListener(mainActionListener);
+		loadMain.setAlignmentX(Component.CENTER_ALIGNMENT);
+		JButton loadConfig = new JButton("Einstellungen");
 		loadConfig.setActionCommand("config");
 		loadConfig.addActionListener(mainActionListener);
 		loadConfig.setAlignmentX(Component.CENTER_ALIGNMENT);
-		exit = new JButton("Beenden");
+		JButton exit = new JButton("Beenden");
 		exit.setActionCommand("exit");
 		exit.addActionListener(mainActionListener);
 		exit.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+		modPanel.add(loadMain);
 		modPanel.add(loadConfig);
 		modPanel.add(Box.createVerticalGlue());
 		modPanel.add(exit);
