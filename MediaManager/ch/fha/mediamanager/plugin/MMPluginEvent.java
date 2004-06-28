4e911c565a45158fd4a167926eb543418b080f54
ia02vond <ia02vond>
2004-06-28 21:10:21 +0000

    added constructors

--- MediaManager/ch/fha/mediamanager/plugin/MMPluginEvent.java
+++ MediaManager/ch/fha/mediamanager/plugin/MMPluginEvent.java
@@ -1,16 +1,18 @@
 package ch.fha.mediamanager.plugin;
 
 import ch.fha.mediamanager.data.DataElement;
+import ch.fha.mediamanager.data.DataSet;
 import ch.fha.mediamanager.data.MetaEntity;
 import ch.fha.pluginstruct.*;
 
 /**
  * @author ia02vond
- * @version $Id: MMPluginEvent.java,v 1.3 2004/06/28 11:24:45 ia02vond Exp $
+ * @version $Id: MMPluginEvent.java,v 1.4 2004/06/28 21:10:21 ia02vond Exp $
  */
 public class MMPluginEvent extends PluginEvent {
 	
 	private DataElement dataElement;
+	private DataSet     dataSet;
 	
 	public MMPluginEvent() {
 	}
@@ -19,12 +21,26 @@ public class MMPluginEvent extends PluginEvent {
 		this.dataElement = dataElement;
 	}
 	
+	public MMPluginEvent(DataSet dataSet) {
+		this.dataSet = dataSet;
+	}
+	
 	public DataElement getDataElement() {
-		return dataElement;
+		return dataElement;		
+	}
+	
+	public DataSet getDataSet() {
+		return dataSet;
 	}
 	
 	public MetaEntity getMetaEntity() {
-		return dataElement.getMetaEntity();
+		if (dataElement != null) {
+			return dataElement.getMetaEntity();
+		} else if (dataSet != null) {
+			return dataSet.getMetaEntity();
+		} else {
+			return null;
+		}
 	}
 
 }
