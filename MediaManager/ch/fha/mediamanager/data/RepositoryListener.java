9442b4f759ae28427e7e92a8ecc1e7ffc73c91d1
ia02vond <ia02vond>
2004-06-28 14:03:25 +0000

    initial import

---
+++ MediaManager/ch/fha/mediamanager/data/RepositoryListener.java
@@ -0,0 +1,11 @@
+package ch.fha.mediamanager.data;
+
+/**
+ * @author ia02vond
+ * @version $Id: RepositoryListener.java,v 1.1 2004/06/28 14:03:25 ia02vond Exp $
+ */
+public interface RepositoryListener {
+
+	public void dataChanged(MetaEntity metaEntity);
+	
+}
