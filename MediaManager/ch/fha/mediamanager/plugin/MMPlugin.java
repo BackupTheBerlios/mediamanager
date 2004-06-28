1cc1fd48a8c7e667eef59070051718cee3026030
ia02vond <ia02vond>
2004-06-28 13:07:43 +0000

    added methods add- / removePanel()

--- MediaManager/ch/fha/mediamanager/plugin/MMPlugin.java
+++ MediaManager/ch/fha/mediamanager/plugin/MMPlugin.java
@@ -1,10 +1,14 @@
 package ch.fha.mediamanager.plugin;
 
+import javax.swing.JPanel;
+
+import ch.fha.mediamanager.gui.MainFrame;
+import ch.fha.mediamanager.gui.components.BarBorder;
 import ch.fha.pluginstruct.*;
 
 /**
  * @author ia02vond
- * @version $Id: MMPlugin.java,v 1.2 2004/06/28 11:24:45 ia02vond Exp $
+ * @version $Id: MMPlugin.java,v 1.3 2004/06/28 13:07:43 ia02vond Exp $
  */
 public abstract class MMPlugin extends AbstractPlugin {
 	
@@ -12,6 +16,15 @@ public abstract class MMPlugin extends AbstractPlugin {
 	
 	public boolean run(PluginEvent event) {
 		return run( (MMPluginEvent)event );
+	}
+	
+	public void addPanel(JPanel panel) {
+		panel.setBorder(new BarBorder(getName()));
+		MainFrame.getInstance().loadCoverPanel(panel);
+	}
+	
+	public void removePanel() {
+		MainFrame.getInstance().removeCoverPanel();
 	}
 	
 }
