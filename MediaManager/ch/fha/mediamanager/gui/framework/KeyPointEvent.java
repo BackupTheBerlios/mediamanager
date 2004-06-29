21982036402d2261c485b41649712969be04a6d9
radisli <radisli>
2004-06-29 13:35:03 +0000

    New Event for database disconnect on program close implemented

--- MediaManager/ch/fha/mediamanager/gui/framework/KeyPointEvent.java
+++ MediaManager/ch/fha/mediamanager/gui/framework/KeyPointEvent.java
@@ -1,4 +1,4 @@
-//$Id: KeyPointEvent.java,v 1.4 2004/06/28 14:01:58 radisli Exp $
+//$Id: KeyPointEvent.java,v 1.5 2004/06/29 13:35:03 radisli Exp $
 package ch.fha.mediamanager.gui.framework;
 
 /**
@@ -9,15 +9,16 @@ package ch.fha.mediamanager.gui.framework;
 public class KeyPointEvent {
 	/** Key Points */
 	public static final int WINDOW_EXIT = 1;
-	public static final int CONFIG_PANEL_LOAD = 2;
-	public static final int PRE_CONNECT = 3;
-	public static final int CONNECT = 4;
-	public static final int POST_CONNECT = 5;
-	public static final int PRE_DISCONNECT = 6;
-	public static final int DISCONNECT = 7;
-	public static final int POST_DISCONNECT = 8;
-	public static final int CONNECT_ERROR = 9;
-	public static final int DISCONNECT_ERROR = 10;
+	public static final int WINDOW_FINAL_EXIT = 2;
+	public static final int CONFIG_PANEL_LOAD = 3;
+	public static final int PRE_CONNECT = 4;
+	public static final int CONNECT = 5;
+	public static final int POST_CONNECT = 6;
+	public static final int PRE_DISCONNECT = 7;
+	public static final int DISCONNECT = 8;
+	public static final int POST_DISCONNECT = 9;
+	public static final int CONNECT_ERROR = 10;
+	public static final int DISCONNECT_ERROR = 11;
 	
 	/** Event responds only on this action */
 	public final int alertOnKey;
